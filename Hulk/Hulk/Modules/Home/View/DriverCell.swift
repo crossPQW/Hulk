d905d10e7c0c6b53d5a1ad81929f34c874bcebbf
黄少华 <651024752@qq.com>
2017-01-05 19:12:01 +0800

    可以显示种子title了

---
+++ Hulk/Hulk/Modules/Home/View/DriverCell.swift
@@ -0,0 +1,32 @@
+//
+//  DriverCell.swift
+//  Hulk
+//
+//  Created by 黄少华 on 2017/1/5.
+//  Copyright © 2017年 黄少华. All rights reserved.
+//
+
+import UIKit
+
+struct Torrent {
+    let title: String
+    let magnets: String
+}
+
+class DriverCell: UITableViewCell {
+
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var magnets: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Hulk/Hulk/ViewController.swift
+++ Hulk/Hulk/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import SnapKit
+import Alamofire
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    let cellID = "DriverCell"
+    var tableView :UITableView!
+    var magnets : Array<String> = []
+    
+    //MARK:- lifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupSubviews()
+        fetchData()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func setupSubviews()  {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = 60
+        tableView.register(UINib(nibName: cellID, bundle: nil), forCellReuseIdentifier: cellID)
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints { (make) in
+            make.edges.equalTo(view)
+        }
     }
-
-
+    
+    func fetchData() {
+        Alamofire.request("https://raw.githubusercontent.com/Chion82/hello-old-driver/master/resource_list.json").responseJSON { (response) in
+            var tempArray: Array<Dictionary<String, Any>> = []
+            if let JSON = response.result.value {
+                tempArray = JSON as! Array
+                for item  in tempArray {
+                    let dic = item 
+                    let string = dic["title"]
+                    self.magnets.append(string! as! String)
+                }
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    //MARK:- UITableViewDelegate & UITableViewDataSource
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return magnets.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as! DriverCell
+        cell.title.text = magnets[indexPath.row]
+        
+        return cell
+    }
 }
 
