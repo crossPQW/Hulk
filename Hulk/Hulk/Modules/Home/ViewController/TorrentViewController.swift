aed9715776dece8f9b063785fce5ea4b39f0ed47
黄少华 <651024752@qq.com>
2017-01-06 20:57:25 +0800

    添加torrentviewcontroller sb

---
+++ Hulk/Hulk/Modules/Home/ViewController/TorrentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TorrentViewController.swift
+//  Hulk
+//
+//  Created by 黄少华 on 2017/1/6.
+//  Copyright © 2017年 黄少华. All rights reserved.
+//
+
+import UIKit
+
+class TorrentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
