e19b105dc666ede2aed80ee5517d8db0d042b886
crossPQW <crossPQW@users.noreply.github.com>
2017-12-29 22:13:35 +0800

    Update BaseNavigationController.swift

--- Hulk/Hulk/Base/BaseNavigationController.swift
+++ Hulk/Hulk/Base/BaseNavigationController.swift
@@ -12,7 +12,7 @@ class BaseNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var a = ""
         // Do any additional setup after loading the view.
     }
 
